0b5dcffaf3b452c326ebd57d1ce5fc25d2bca58a
unetamps <unedo.tampubolon@gmail.com>
2024-10-16 01:37:33 +0700

    add auth service

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,5 +21,10 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Model::preventLazyLoading();
+        \Illuminate\Foundation\Http\Middleware\VerifyCsrfToken::except(
+            [
+            'submit'
+            ]
+        );
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -6,21 +6,27 @@ use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/',[ProductController::class,'getProduct']);
+Route::get('/', [ProductController::class,'getProduct']);
 
-
-
-Route::get(
-    "/logout",
+Route::prefix('user')->group(
     function () {
-        Auth::logout();
-        return redirect()->intended('login');
+        Route::controller(UserController::class)->group(
+            function () {
+                Route::get('/register', 'viewRegister');
+                Route::post('/register', 'register');
+                Route::post('/login', 'login');
+                Route::get('/login', 'viewLogin');
+                Route::post('/logout', 'logout');
+            }
+        );
     }
 );
+
+
 Route::get('/product/{product}', [ProductController::class, 'getProduct'])->middleware('user');
 
-Route::post('/register', [UserController::class, 'register']);
-Route::post('/login', [UserController::class, 'login']);
+/* Route::post('/register', [UserController::class, 'register']); */
+/* Route::post('/login', [UserController::class, 'login']); */
 Route::get(
     '/productpics',
     function () {
@@ -29,17 +35,3 @@ Route::get(
 );
 Route::post('/productpics', [ProductPictureController::class, 'upload']);
 
-Route::get(
-    "/register",
-    function () {
-        return view('register');
-    }
-);
-
-Route::get(
-    "/login",
-    function () {
-        // dd(Auth::user());
-        return view('login');
-    }
-);
